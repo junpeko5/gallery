9b078aba015103d4dba2d7cdcc7d751129400ecc
Junpei Konishi <junpeko5@gmail.com>
2018-09-19 22:37:52 +0900

    Improving our Connection to be more OOP

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -12,27 +12,37 @@ class Database {
 
     public function open_db_connection() {
 
-        $this->connection = mysqli_connect(DB_HOST, DB_USER ,DB_PASS, DB_NAME);
+//        $this->connection = mysqli_connect(DB_HOST, DB_USER ,DB_PASS, DB_NAME);
 
-        if (mysqli_connect_errno()) {
-            die("Database connection failed badly" . mysqli_error());
+        $this->connection = new mysqli(DB_HOST, DB_USER ,DB_PASS, DB_NAME);
+
+
+        if ($this->connection->connect_errno) {
+            die("Database connection failed badly" . $this->connection->connect_error);
         }
     }
 
     public function query($sql) {
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+        $this->confirm_query($result);
         return $result;
     }
 
     private function confirm_query($result) {
+
+
         if (!$result) {
-            die("Query Failed");
+            die("Query Failed" . $this->connection->error);
         }
     }
 
     public function escape_string($string) {
-        $escaped_string = mysqli_real_escape_string($this->connection, $stirng);
+        $escaped_string = $this->connection->real_escape_string($string);
         return $escaped_string;
+    }
+
+    public function the_insert_id() {
+        return $this->connection->insert_id;
     }
 }
 
