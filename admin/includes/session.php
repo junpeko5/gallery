279707f0b2fbddbb9d41e0f34674eba19d75377d
Junpei Konishi <junpeko5@gmail.com>
2018-09-21 22:22:23 +0900

    The Login Method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,12 +2,25 @@
 
 class Session {
 
-    private $signed_in;
+    private $signed_in = false;
     public $user_id;
 
     function __construct() {
         session_start();
+        $this->check_the_login();
     }
+
+    public function is_signed_in() {
+        return $this->signed_in;
+    }
+
+    public function login($user) {
+        if ($user) {
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+        }
+    }
+
 
     private function check_the_login() {
         if (isset($_SESSION['user_id'])) {
