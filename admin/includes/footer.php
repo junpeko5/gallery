4a91070c512dd4170604cee52856fbfcc6cdee99
Junpei Konishi <junpeko5@gmail.com>
2018-09-24 15:11:29 +0900

    Installing The Text Editor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,11 +2,14 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="/gallery/js/jquery.js"></script>
+    <script src="/gallery/admin/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="/gallery/js/bootstrap.min.js"></script>
+    <script src="/gallery/admin/js/bootstrap.min.js"></script>
 
+  <!-- WYSIWYG -->
+  <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=xmqyusgt0kob2jl1gixmom3rmxs69n4b874l8c8ninnkrj5l"></script>
+  <script src="js/scripts.js"></script>
 </body>
 
 </html>
