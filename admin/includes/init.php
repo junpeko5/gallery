48be11eb569f674727bb55ca30c329a7f2d7589b
Junpei Konishi <junpeko5@gmail.com>
2018-09-26 22:37:37 +0900

    Including Our Classes in Photo.php

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -4,10 +4,10 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . DS . 'gallery');
 defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT . DS . 'admin' . DS . 'includes');
 
-require_once("functions.php");
-require_once("config.php");
-require_once("database.php");
-require_once("db_object.php");
-require_once("user.php");
-require_once("photo.php");
-require_once("session.php");
+require_once(dirname(__FILE__) . "/functions.php");
+require_once(dirname(__FILE__) . "/config.php");
+require_once(dirname(__FILE__) . "/database.php");
+require_once(dirname(__FILE__) . "/db_object.php");
+require_once(dirname(__FILE__) . "/user.php");
+require_once(dirname(__FILE__) . "/photo.php");
+require_once(dirname(__FILE__) . "/session.php");
