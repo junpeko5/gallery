9c820660aa662aa2a7cd8a59c0e30f0875d36557
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 08:42:20 +0900

    Displaying & Making Comments -BACK-END

--- admin/comments.php
+++ admin/comments.php
@@ -1,4 +1,11 @@
-<?php include("includes/header.php"); ?>
+<?php
+include("includes/header.php");
+if (!$session->is_signed_in()) {
+    redirect("login.php");
+}
+$comments = Comment::find_all();
+
+?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -19,17 +26,34 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                        Comments
-                        <small>Subheading</small>
+                        comments
                     </h1>
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
+                    <a href="add_comment.php" class="btn btn-primary">Add comment</a>
+                    <div class="col-md-12">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Author</th>
+                                <th>body</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($comments as $comment) : ?>
+                                <tr>
+                                    <td><?php echo $comment->id; ?></td>
+                                    <td>
+                                        <?php echo $comment->author; ?>
+                                        <div class="action_links">
+                                            <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>
+                                        </div>
+                                    </td>
+                                    <td><?php echo $comment->body; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table> <!-- End of Table -->
+                    </div>
                 </div>
             </div>
             <!-- /.row -->
