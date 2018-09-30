c00c1334edbfc502aeec056dbb10fb3e89a40b00
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 16:58:53 +0900

    Paginate Indication and Looping

--- index.php
+++ index.php
@@ -33,16 +33,25 @@ $photos = Photo::find_by_query($sql);
         </div>
         <div class="row">
             <ul class="pager">
-                <?php if ($paginate->page_total() > 1) : ?>
-                    <?php if ($paginate->has_previous()) : ?>
-                        <li class="previous"><a href="index.php?page=<?php echo $paginate->previous(); ?>">Previous</a></li>
-                    <?php endif; ?>
-                <?php endif; ?>
-                <?php if ($paginate->page_total() > 1) : ?>
-                    <?php if ($paginate->has_next()) : ?>
-                        <li class="next"><a href="index.php?page=<?php echo $paginate->next(); ?>">Next</a></li>
-                    <?php endif; ?>
-                <?php endif; ?>
+                <?php
+                if ($paginate->page_total() > 1) {
+                    if ($paginate->has_previous()) {
+                        echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                    }
+                    for ($i = 1; $i <= $paginate->page_total(); $i++) {
+                        if ($i == $paginate->current_page) {
+                            echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                        }
+
+                    }
+
+                    if ($paginate->has_next()) {
+                        echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                    }
+                }
+                ?>
+<!--                -->
+
             </ul>
         </div>
     </div>
