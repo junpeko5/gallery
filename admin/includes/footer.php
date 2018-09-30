55e6d542d4e572b115be7e02326f81dc161aeb68
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 11:29:52 +0900

    Adding Google API Charts

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -10,6 +10,31 @@
   <!-- WYSIWYG -->
   <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=xmqyusgt0kob2jl1gixmom3rmxs69n4b874l8c8ninnkrj5l"></script>
   <script src="js/scripts.js"></script>
+
+  <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+          var data = google.visualization.arrayToDataTable([
+              ['Task', 'Hours per Day'],
+              ['Work',     11],
+              ['Eat',      2],
+              ['Commute',  2],
+              ['Watch TV', 2],
+              ['Sleep',    7]
+          ]);
+
+          var options = {
+              title: 'My Daily Activities'
+          };
+
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+          chart.draw(data, options);
+      }
+  </script>
 </body>
 
 </html>
