b046ec28f0f5887a359951d895c95915e37c89cb
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 20:52:02 +0900

    Pulling Pictures Into Gallery / Modal

--- admin/includes/photo_library_modal.php
+++ admin/includes/photo_library_modal.php
@@ -1,3 +1,8 @@
+<?php require_once("init.php"); ?>
+<?php
+$photos = Photo::find_all(); ?>
+
+?>
 <div class="modal fade" id="photo-library">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -9,16 +14,14 @@
                 <div class="col-md-9">
                     <div class="thumbnails row">
 
-                        <!-- PHP LOOP HERE CODE HERE-->
-
+                        <?php foreach ($photos as $photo) : ?>
                         <div class="col-xs-2">
                             <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
-                                <img class="modal_thumbnails img-responsive" src="" data="">
+                                <img class="modal_thumbnails img-responsive" src="<?php echo $photo->picture_path(); ?>" data="">
                             </a>
                             <div class="photo-id hidden"></div>
                         </div>
-
-                        <!-- PHP LOOP HERE CODE HERE-->
+                        <?php endforeach; ?>
 
                     </div>
                 </div><!--col-md-9 -->
