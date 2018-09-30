afb98935e7dbcf9c5977c6151439d4cd4a6e0566
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 12:43:05 +0900

    Dynamic Data in Chart Creation

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                Admin
+                Admin Dashboard
                 <small>Subheading</small>
             </h1>
 
