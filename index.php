80ef79c37f58828d4bd55f083d370095d0e27f1d
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 14:53:27 +0900

    correcting photos alignment with css

--- index.php
+++ index.php
@@ -8,10 +8,9 @@ $photos = Photo::find_all();
     <div class="col-md-12">
         <div class="thumbnails row">
         <?php foreach ($photos as $photo) : ?>
-
             <div class="col-xs-6 col-md-3">
                 <a class="thumbnail" href="">
-                    <img src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                    <img class="img-responsive home_page_photo" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
                 </a>
             </div>
         <?php endforeach; ?>
