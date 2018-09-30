664fe4c2a82687b8fe301bd65ff1fa2aa26256fe
Junpei Konishi <junpeko5@gmail.com>
2018-09-30 15:03:07 +0900

    Coding the PHP in Photo.php FRONTEND

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $photos = Photo::find_all();
         <div class="thumbnails row">
         <?php foreach ($photos as $photo) : ?>
             <div class="col-xs-6 col-md-3">
-                <a class="thumbnail" href="">
+                <a class="thumbnail" href="photo.php?id=<?php echo $photo->id; ?>">
                     <img class="img-responsive home_page_photo" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
                 </a>
             </div>
